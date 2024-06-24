65de9bcdd8e031bf8c2e43ada1117d604e578f6d
Gustav Grusell <gustav.grusell@eyevinn.se>
2024-06-24 22:15:33 +0200

    feat: support s3 urls for input and output
    This commit add support for using s3 urls on the format
    s3://<BUCKET>/<KEY> in both input and output.
    
    If ans s3 URL is used as input, a presigned URL is created
    and used as input to ffmpeg. The duration of the presigned URLs can be
    controlled with the 'remote-files.s3.presignDurationSeconds' config
    property.
    
    If an s3 URL is used for 'outputFolder', output will first be stored
    locally and then uploaded to s3 once transcoding is finished.
    
    Aws credentials are read with DefaultCredentialsProvider, meaning
    aws credentials can be provided in a number of ways, see
    https://sdk.amazonaws.com/java/api/latest/software/amazon/awssdk/auth/credentials/DefaultCredentialsProvider.html;
    
    Not that when using s3 urls for input, the presigned URLs will be
    shown in the logs. If this is not desirable, setting
    logging.config (or env variable LOGGING_CONFIG) to
    'classpath:logback-json-mask-s3-presign.xml'
    will use a log config that masks the presign query parameters.
    
    By setting env variable REMOTEFILES_S3_ANONYMOUSACCESS to true, s3 urls will be
    accessed in anonymous mode, corresponding to using the '--no-sign-request' flag
    with the aws cli. Any s3 access key or secrets key configured will be
    ignored. Multipart upload will be disabled in this case since the s3
    sdk does not support multipart upload when using anonymous access.
    
    Signed-off-by: Gustav Grusell <gustav.grusell@eyevinn.se>

--- encore-common/build.gradle.kts
+++ encore-common/build.gradle.kts
@@ -16,6 +16,8 @@ dependencies {
 
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-slf4j")
+    implementation(platform("software.amazon.awssdk:bom:2.29.2"))
+    implementation("software.amazon.awssdk:s3")
 
     testImplementation(project(":encore-web"))
     testImplementation("org.springframework.security:spring-security-test")
@@ -26,6 +28,7 @@ dependencies {
     testFixturesImplementation("com.redis:testcontainers-redis:2.2.4")
     testFixturesImplementation("io.github.microutils:kotlin-logging:3.0.5")
     testFixturesImplementation("org.junit.jupiter:junit-jupiter-api")
+    testFixturesImplementation("org.testcontainers:localstack:1.20.3")
     testFixturesRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
---
+++ encore-common/src/main/kotlin/se/svt/oss/encore/S3RemoteFilesConfiguration.kt
@@ -0,0 +1,81 @@
+// SPDX-FileCopyrightText: 2024 Eyevinn Technology AB
+//
+// SPDX-License-Identifier: EUPL-1.2
+
+package se.svt.oss.encore
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import se.svt.oss.encore.service.remotefiles.s3.S3Properties
+import se.svt.oss.encore.service.remotefiles.s3.S3RemoteFileHandler
+import se.svt.oss.encore.service.remotefiles.s3.S3UriConverter
+import software.amazon.awssdk.auth.credentials.AnonymousCredentialsProvider
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider
+import software.amazon.awssdk.regions.Region
+import software.amazon.awssdk.services.s3.S3AsyncClient
+import software.amazon.awssdk.services.s3.S3Configuration
+import software.amazon.awssdk.services.s3.presigner.S3Presigner
+import java.net.URI
+
+@ConditionalOnProperty("remote-files.s3.enabled", havingValue = "true")
+@EnableConfigurationProperties(S3Properties::class)
+@Configuration
+class S3RemoteFilesConfiguration {
+
+    @Bean
+    fun s3Region() =
+        Region.of(System.getProperty("aws.region") ?: System.getenv("AWS_REGION") ?: "us-east-1")
+
+    @Bean
+    fun s3Client(s3Region: Region, s3Properties: S3Properties) = S3AsyncClient.builder()
+        .region(s3Region)
+        .crossRegionAccessEnabled(true)
+        .multipartEnabled(!s3Properties.anonymousAccess) // Multipart upload requires credentials
+        .serviceConfiguration(
+            S3Configuration.builder()
+                .pathStyleAccessEnabled(true)
+                .build(),
+        )
+        .credentialsProvider(
+            if (s3Properties.anonymousAccess) {
+                AnonymousCredentialsProvider.create()
+            } else {
+                DefaultCredentialsProvider.create()
+            },
+        )
+        .apply {
+            if (!s3Properties.endpoint.isNullOrBlank()) {
+                endpointOverride(URI.create(s3Properties.endpoint))
+            }
+        }
+        .build()
+
+    @Bean
+    fun s3Presigner(s3Region: Region, s3Properties: S3Properties) = S3Presigner.builder()
+        .region(s3Region)
+        .serviceConfiguration(
+            S3Configuration.builder()
+                .pathStyleAccessEnabled(true)
+                .build(),
+        )
+        .apply {
+            if (!s3Properties.endpoint.isNullOrBlank()) {
+                endpointOverride(URI.create(s3Properties.endpoint))
+            }
+        }
+        .build()
+
+    @Bean
+    fun s3UriConverter(s3Properties: S3Properties, s3Region: Region) = S3UriConverter(s3Properties, s3Region)
+
+    @Bean
+    fun s3RemoteFileHandler(
+        s3Client: S3AsyncClient,
+        s3Presigner: S3Presigner,
+        s3Properties: S3Properties,
+        s3UriConverter: S3UriConverter,
+    ) =
+        S3RemoteFileHandler(s3Client, s3Presigner, s3Properties, s3UriConverter)
+}
