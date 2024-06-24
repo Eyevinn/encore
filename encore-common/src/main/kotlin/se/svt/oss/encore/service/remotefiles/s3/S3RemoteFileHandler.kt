3613f78fe173ee7a24457f500766e0ebd6a93c20
Gustav Grusell <gustav.grusell@eyevinn.se>
2024-06-24 22:15:33 +0200

    feat: support s3 urls for input and output
    This commit add support for using s3 urls on the format
    s3://<BUCKET>/<KEY> in both input and output.
    
    If ans s3 URL is used as input, a presigned URL is created
    and used as input to ffmpeg. The duration of the presigned URLs can be
    controlled with the 'remote-files.s3.presignDurationSeconds' config
    property.
    
    If an s3 URL is used for 'outputFolder', output will first be stored
    locally and then uploaded to s3 once transcoding is finished.
    
    Aws credentials are read with DefaultCredentialsProvider, meaning
    aws credentials can be provided in a number of ways, see
    https://sdk.amazonaws.com/java/api/latest/software/amazon/awssdk/auth/credentials/DefaultCredentialsProvider.html;
    
    Not that when using s3 urls for input, the presigned URLs will be
    shown in the logs. If this is not desirable, setting
    logging.config (or env variable LOGGING_CONFIG) to
    'classpath:logback-json-mask-s3-presign.xml'
    will use a log config that masks the presign query parameters.
    
    By setting env variable REMOTEFILES_S3_ANONYMOUSACCESS to true, s3 urls will be
    accessed in anonymous mode, corresponding to using the '--no-sign-request' flag
    with the aws cli. Any s3 access key or secrets key configured will be
    ignored. Multipart upload will be disabled in this case since the s3
    sdk does not support multipart upload when using anonymous access.
    
    Signed-off-by: Gustav Grusell <gustav.grusell@eyevinn.se>

---
+++ encore-common/src/main/kotlin/se/svt/oss/encore/service/remotefiles/s3/S3RemoteFileHandler.kt
@@ -0,0 +1,66 @@
+// SPDX-FileCopyrightText: 2024 Eyevinn Technology AB
+//
+// SPDX-License-Identifier: EUPL-1.2
+
+package se.svt.oss.encore.service.remotefiles.s3
+
+import io.github.oshai.kotlinlogging.KotlinLogging
+import se.svt.oss.encore.service.remotefiles.RemoteFileHandler
+import software.amazon.awssdk.services.s3.S3AsyncClient
+import software.amazon.awssdk.services.s3.model.GetObjectRequest
+import software.amazon.awssdk.services.s3.model.PutObjectRequest
+import software.amazon.awssdk.services.s3.presigner.S3Presigner
+import software.amazon.awssdk.services.s3.presigner.model.GetObjectPresignRequest
+import java.net.URI
+import java.nio.file.Paths
+import java.util.concurrent.TimeUnit
+
+private val log = KotlinLogging.logger {}
+
+class S3RemoteFileHandler(
+    private val client: S3AsyncClient,
+    private val presigner: S3Presigner,
+    private val s3Properties: S3Properties,
+    private val s3UriConverter: S3UriConverter,
+) : RemoteFileHandler {
+
+    override fun getAccessUri(uri: String): String {
+        val s3Uri = URI.create(uri)
+
+        if (s3Properties.anonymousAccess) {
+            return s3UriConverter.toHttp(s3Uri)
+        }
+        return presignUrl(s3Uri)
+    }
+
+    private fun presignUrl(s3Uri: URI): String {
+        val (bucket, key) = s3UriConverter.getBucketAndKey(s3Uri)
+
+        val objectRequest: GetObjectRequest = GetObjectRequest.builder()
+            .bucket(bucket)
+            .key(key)
+            .build()
+        val presignRequest: GetObjectPresignRequest = GetObjectPresignRequest.builder()
+            .signatureDuration(java.time.Duration.ofSeconds(s3Properties.presignDurationSeconds))
+            .getObjectRequest(objectRequest)
+            .build()
+
+        val presignedRequest = presigner.presignGetObject(presignRequest)
+        val url = presignedRequest.url().toExternalForm()
+        return url
+    }
+
+    override fun upload(localFile: String, remoteFile: String) {
+        log.info { "Uploading $localFile to $remoteFile" }
+        val s3Uri = URI.create(remoteFile)
+        val (bucket, key) = s3UriConverter.getBucketAndKey(s3Uri)
+        val putObjectRequest: PutObjectRequest = PutObjectRequest.builder()
+            .bucket(bucket)
+            .key(key)
+            .build()
+        val res = client.putObject(putObjectRequest, Paths.get(localFile)).get(s3Properties.presignDurationSeconds, TimeUnit.SECONDS)
+        log.info { "Upload result: $res" }
+    }
+
+    override val protocols = listOf("s3")
+}
