52d54cf53a44f0b6ff596f06e0714245f55308df
Gustav Grusell <gustav.grusell@eyevinn.se>
2025-05-16 15:33:02 +0200

    feat: support for specifiying custom filters for split,scale,crop,pad
    When doing hardware encoding, it can be desirable to use hardware
    filters for split, scaling, cropping and padding. This commits add support
    for specifying replacements for the standard filters in a profile.
    It is required that the filter specified supports the parameters used
    by encore.
    
    Signed-off-by: Gustav Grusell <gustav.grusell@eyevinn.se>

--- encore-common/src/main/kotlin/se/svt/oss/encore/model/profile/Profile.kt
+++ encore-common/src/main/kotlin/se/svt/oss/encore/model/profile/Profile.kt
@@ -10,5 +10,13 @@ data class Profile(
     val encodes: List<OutputProducer>,
     val scaling: String? = "bicubic",
     val deinterlaceFilter: String = "yadif",
+    val filterSettings: FilterSettings = FilterSettings(),
     val joinSegmentParams: LinkedHashMap<String, Any?> = linkedMapOf(),
+)
+
+data class FilterSettings(
+    val splitFilter: String = "split",
+    val scaleFilter: String = "scale",
+    val cropFilter: String = "crop",
+    val padFilter: String = "pad",
 )
--- encore-common/src/main/kotlin/se/svt/oss/encore/model/profile/VideoEncode.kt
+++ encore-common/src/main/kotlin/se/svt/oss/encore/model/profile/VideoEncode.kt
@@ -28,9 +28,9 @@ interface VideoEncode : OutputProducer {
     val codec: String
     val inputLabel: String
 
-    override fun getOutput(job: EncoreJob, encodingProperties: EncodingProperties): Output? {
+    override fun getOutput(job: EncoreJob, encodingProperties: EncodingProperties, filterSettings: FilterSettings): Output? {
         val audioEncodesToUse = audioEncodes.ifEmpty { listOfNotNull(audioEncode) }
-        val audio = audioEncodesToUse.flatMap { it.getOutput(job, encodingProperties)?.audioStreams.orEmpty() }
+        val audio = audioEncodesToUse.flatMap { it.getOutput(job, encodingProperties, filterSettings)?.audioStreams.orEmpty() }
         val videoInput = job.inputs.videoInput(inputLabel)
             ?: throw RuntimeException("No valid video input with label $inputLabel!")
         return Output(
@@ -40,7 +40,7 @@ interface VideoEncode : OutputProducer {
                 firstPassParams = firstPassParams().toParams(),
                 inputLabels = listOf(inputLabel),
                 twoPass = twoPass,
-                filter = videoFilter(job.debugOverlay, encodingProperties, videoInput),
+                filter = videoFilter(job.debugOverlay, encodingProperties, videoInput, filterSettings),
             ),
             audioStreams = audio,
             output = "${job.baseName}$suffix.$format",
@@ -66,6 +66,7 @@ interface VideoEncode : OutputProducer {
         debugOverlay: Boolean,
         encodingProperties: EncodingProperties,
         videoInput: VideoIn,
+        filterSettings: FilterSettings,
     ): String? {
         val videoFilters = mutableListOf<String>()
         var scaleToWidth = width
@@ -83,10 +84,10 @@ interface VideoEncode : OutputProducer {
             scaleToHeight = width
         }
         if (scaleToWidth != null && scaleToHeight != null) {
-            videoFilters.add("scale=$scaleToWidth:$scaleToHeight:force_original_aspect_ratio=decrease:force_divisible_by=2")
+            videoFilters.add("${filterSettings.scaleFilter}=$scaleToWidth:$scaleToHeight:force_original_aspect_ratio=decrease:force_divisible_by=2")
             videoFilters.add("setsar=1/1")
         } else if (scaleToWidth != null || scaleToHeight != null) {
-            videoFilters.add("scale=${scaleToWidth ?: -2}:${scaleToHeight ?: -2}")
+            videoFilters.add("${filterSettings.scaleFilter}=${scaleToWidth ?: -2}:${scaleToHeight ?: -2}")
         }
         filters?.let { videoFilters.addAll(it) }
         if (debugOverlay) {
